d34a2fdb4989c4459f2162349bac66de4afe5d25
Cariad Eccleston <cariad@cariad.earth>
2021-11-14 09:11:07 +0000

    Use mdcode

--- dinject/inject.py
+++ dinject/inject.py
@@ -1,17 +1,19 @@
 from pathlib import Path
-from re import match
 from shutil import move
 from subprocess import run
 from tempfile import NamedTemporaryFile
-from typing import IO, Optional
+from typing import IO, Iterable, Optional, Union
 
+from mdcode import Block, LineReader
 from naughtty import NaughTTY
 from thtml import Scope, write_html
 
 from dinject.enums import Content, Host, Range
 from dinject.executors import get_executor
 from dinject.parser import Parser
-from dinject.types import Block, Instruction
+from dinject.types import Instruction
+
+Reader = Union[str, IO[str]]
 
 
 def execute(
@@ -29,7 +31,7 @@ def execute(
 
     if not executor:
         # We don't support this language, so pass through.
-        block.write(writer)
+        block.render(writer)
         return
 
     if instruction.host == Host.TERMINAL:
@@ -40,61 +42,61 @@ def execute(
         process = run(executor.arguments, capture_output=True)
         content = process.stdout.decode("UTF-8")
 
-    content = content.rstrip() + "\n"
+    content = content.rstrip()
 
     parser.write_range_start(instruction, writer)
     writer.write("\n")
 
     if instruction.content == Content.HTML:
-        write_html(text=content, writer=writer, scope=Scope.FRAGMENT, theme="plain")
+        write_html(
+            text=content + "\n",
+            writer=writer,
+            scope=Scope.FRAGMENT,
+            theme="plain",
+        )
         writer.write("\n")
     else:
-        Block(lang="text", lines=[content]).write(writer)
+        Block(lang="text", lines=content.split("\n")).render(writer)
 
     writer.write("\n")
     parser.write_range_end(writer)
 
 
-def inject(reader: IO[str], writer: IO[str], parser: Optional[Parser] = None) -> None:
+def inject(
+    reader: Reader,
+    writer: IO[str],
+    parser: Optional[Parser] = None,
+) -> None:
     """Reads and injects from `reader` to `writer`."""
 
-    block: Optional[Block] = None
+    line_reader = LineReader()
     parser = parser or Parser()
     skip_to_emitted_end = False
 
-    for line in reader:
-        if not block or block.complete:
-            din = parser.get_instruction(line)
+    for line in iterate_lines(reader):
+        if not skip_to_emitted_end:
+            line_reader.read(line)
 
-            if skip_to_emitted_end:
-                if din and din.range == Range.END:
-                    skip_to_emitted_end = False
-                continue
+        din = parser.get_instruction(line)
 
-            if din and block:
-                execute(
-                    block=block,
-                    instruction=din,
-                    parser=parser,
-                    writer=writer,
-                )
-                block = None
-                if din.range == Range.START:
-                    skip_to_emitted_end = True
-                continue
+        if skip_to_emitted_end:
+            if din and din.range == Range.END:
+                skip_to_emitted_end = False
+            continue
+
+        if din and line_reader.complete:
+            execute(
+                block=line_reader.complete,
+                instruction=din,
+                parser=parser,
+                writer=writer,
+            )
+            if din.range == Range.START:
+                skip_to_emitted_end = True
+            continue
 
         writer.write(line)
-
-        if block:
-            if not block.complete:
-                if line == "```\n":
-                    block.complete = True
-                    continue
-
-                block.lines.append(line)
-                continue
-
-        block = is_block_start(line) or block
+        writer.write("\n")
 
 
 def inject_file(path: Path, parser: Optional[Parser] = None) -> None:
@@ -110,12 +112,13 @@ def inject_file(path: Path, parser: Optional[Parser] = None) -> None:
                 reader=reader,
                 writer=writer,
             )
-        writer.flush()
         move(writer.name, path)
 
 
-def is_block_start(line: str) -> Optional[Block]:
-    m = match("^```(.+)$", line)
-    if not m:
-        return None
-    return Block(lang=m.group(1), lines=[], complete=False)
+def iterate_lines(reader: Reader) -> Iterable[str]:
+    """Returns an line iterator."""
+
+    it = reader.split("\n") if isinstance(reader, str) else reader
+
+    for line in it:
+        yield line.rstrip()
